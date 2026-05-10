2e234e328a884cb83c26347528018d314ad4c885
Danna Sharik Cardenas Hernandez <02220131027@mail.udes.edu.co>
2026-05-10 11:56:25 -0500

    test: violacion de arquitectura intencional

--- src/main/java/com/empresa/pedidos/dominio/Pedido.java
+++ src/main/java/com/empresa/pedidos/dominio/Pedido.java
@@ -14,6 +14,7 @@ public class Pedido {
     private Long id;
     private double subtotal;
     private double costo;
+    private final com.empresa.pedidos.infraestructura.persistencia.PedidoJpaRepository repo = null;
 
     @Enumerated(EnumType.STRING)
     private TipoPedido tipo;
